e142d6bf64aff4cbe0f341ddaf292dd10f2ab2b4
Jyothish V S <jsvidyad@syr.edu>
2024-02-10 20:16:01 +0530

    Formatted the code.

--- src/appellation.rs
+++ src/appellation.rs
@@ -1,15 +1,15 @@
-use std::ops::Drop;
 use std::fmt::Debug;
+use std::ops::Drop;
 
 #[derive(Debug, Clone)]
 pub struct Appellation {
     name: String,
-    nicknames: Vec<String>
+    nicknames: Vec<String>,
 }
 
 impl Appellation {
     pub fn new(name: String, nicknames: Vec<String>) -> Self {
-        Self {name, nicknames}
+        Self { name, nicknames }
     }
 }
 
